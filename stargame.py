b6d85919ef5c48d10d8473d8cafdf42d2d517de7
Niko <nikoloz.xucishvili655@gmail.com>
2024-07-19 20:47:04 +0400

    deleted second background and added player rectangle

--- stargame.py
+++ stargame.py
@@ -2,25 +2,35 @@ import pygame
 import time
 import random
 
-
 WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Dodge")
 
-BG = pygame.image.load("background-2.jpg")
+BG = pygame.transform.scale(pygame.image.load("./background-2.jpg"), (WIDTH, HEIGHT))
 
-def draw():
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 40
+
+
+def draw(player):
     WIN.blit(BG, (0, 0))
+
+    pygame.draw.rect(WIN, "green", player)
+
     pygame.display.update()
+
 
 def main():
     run = True
+
+    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
 
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+        draw(player)
 
     pygame.quit()
 
