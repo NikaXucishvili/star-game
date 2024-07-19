d409c765caed5695d1712fbe3208b553df4d4f36
Niko <nikoloz.xucishvili655@gmail.com>
2024-07-19 21:16:20 +0400

    added player space ship design

--- stargame.py
+++ stargame.py
@@ -2,20 +2,29 @@ import pygame
 import time
 import random
 
+
 WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Dodge")
 
 BG = pygame.transform.scale(pygame.image.load("./background-2.jpg"), (WIDTH, HEIGHT))
 
-PLAYER_WIDTH = 40
-PLAYER_HEIGHT = 40
+PLAYER_WIDTH = 80
+PLAYER_HEIGHT = 80
+
+player_image = pygame.image.load('rocket-2-removed.png').convert_alpha()
+player_image.set_colorkey((55, 155, 100))
+
+
+player_image = pygame.transform.scale(player_image, (PLAYER_WIDTH, PLAYER_HEIGHT))
+
+
 
 
 def draw(player):
     WIN.blit(BG, (0, 0))
 
-    pygame.draw.rect(WIN, "green", player)
+    WIN.blit(player_image, player.topleft)
 
     pygame.display.update()
 
