1a723511e754384fd9dc2b77d65ad8b39db7df71
Niko <nikoloz.xucishvili655@gmail.com>
2024-07-20 22:39:28 +0400

    added startup bg and text

--- stargame.py
+++ stargame.py
@@ -4,32 +4,42 @@ import random
 
 pygame.font.init()
 
-WIDTH, HEIGHT = 1200, 800
+# Screen dimensions
+WIDTH, HEIGHT = 1280, 768
+
+# Initialize the display window
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Dodge")
 
+# Load background images
 BG = pygame.transform.scale(pygame.image.load("background-2.jpg"), (WIDTH, HEIGHT))
-STARTBG = pygame.transform.scale(pygame.image.load("spaceship.jpg"), (WIDTH, HEIGHT))
+STARTBG = pygame.transform.scale(pygame.image.load("image2.jpg"), (WIDTH, HEIGHT))
+
+# Player settings
 PLAYER_WIDTH = 80
 PLAYER_HEIGHT = 80
 PLAYER_VEL = 5
 
+# Star settings
 STAR_WIDTH = 70
 STAR_HEIGHT = 70
 STAR_VEL = 3
 
+# Font settings
 FONT = pygame.font.SysFont("comicsans", 30)
 
+# Load player image
 player_image = pygame.image.load('rocket-2-removed.png')
 player_image = pygame.transform.scale(player_image, (PLAYER_WIDTH, PLAYER_HEIGHT))
 
+# Load star image
 star_image = pygame.image.load("stone-removebg-preview.png")
 star_image = pygame.transform.scale(star_image, (STAR_WIDTH, STAR_HEIGHT))
 
 def draw(player, elapsed_time, stars):
     WIN.blit(BG, (0, 0))
 
-    time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1, "white")
+    time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1, (255, 255, 255))
     WIN.blit(time_text, (10, 10))
 
     WIN.blit(player_image, player.topleft)
@@ -41,26 +51,45 @@ def draw(player, elapsed_time, stars):
 
 def start_screen():
     run = True
+    title_font = pygame.font.SysFont("comicsans", 80)
+    control_font = pygame.font.SysFont("comicsans", 40)
+    instruction_font = pygame.font.SysFont("comicsans", 30)
+
+    title_text = title_font.render("Space Dodge", 1, (255, 255, 255))
+    control_text = control_font.render("Controls:", 1, (255, 255, 255))
+    up_text = instruction_font.render("Up: Move Up", 1, (255, 255, 255))
+    down_text = instruction_font.render("Down: Move Down", 1, (255, 255, 255))
+    left_text = instruction_font.render("Left: Move Left", 1, (255, 255, 255))
+    right_text = instruction_font.render("Right: Move Right", 1, (255, 255, 255))
+    start_text = instruction_font.render("Press any key to start", 1, (255, 255, 255))
+
+    title_text_rect = title_text.get_rect(center=(WIDTH // 2, 150))
+    control_text_rect = control_text.get_rect(topleft=(100, 250))
+    up_text_rect = up_text.get_rect(topleft=(150, 300))
+    down_text_rect = down_text.get_rect(topleft=(150, 350))
+    left_text_rect = left_text.get_rect(topleft=(150, 400))
+    right_text_rect = right_text.get_rect(topleft=(150, 450))
+    start_text_rect = start_text.get_rect(center=(WIDTH // 2, 700))
+
+    anim_count = 0
+    anim_speed = 60
+
     while run:
-        WIN.blit(STARTBG, (0, 0))  # Fill screen with black
+        WIN.blit(STARTBG, (0, 0))
 
-        title_text = FONT.render("Space Dodge", 1, "white")
-        control_text = FONT.render("Controls:", 1, "white")
-        up_text = FONT.render("Up: Move Up", 1, "white")
-        down_text = FONT.render("Down: Move Down", 1, "white")
-        left_text = FONT.render("Left: Move Left", 1, "white")
-        right_text = FONT.render("Right: Move Right", 1, "white")
-        win_condition_text = FONT.render("Survive more than 40 seconds to win!", 1, "white")
-        start_text = FONT.render("Press any key to start", 1, "white")
+        # Blinking effect for "Press any key to start"
+        if anim_count < anim_speed // 2:
+            WIN.blit(start_text, start_text_rect)
+        if anim_count >= anim_speed // 2:
+            anim_count = 0
+        anim_count += 1
 
-        WIN.blit(title_text, (WIDTH/2 - title_text.get_width()/2, 100))
-        WIN.blit(control_text, (100, 250))
-        WIN.blit(up_text, (150, 300))
-        WIN.blit(down_text, (150, 350))
-        WIN.blit(left_text, (150, 400))
-        WIN.blit(right_text, (150, 450))
-        WIN.blit(win_condition_text, (WIDTH/2 - win_condition_text.get_width()/2, 550))
-        WIN.blit(start_text, (WIDTH/2 - start_text.get_width()/2, 700))
+        WIN.blit(title_text, title_text_rect)
+        WIN.blit(control_text, control_text_rect)
+        WIN.blit(up_text, up_text_rect)
+        WIN.blit(down_text, down_text_rect)
+        WIN.blit(left_text, left_text_rect)
+        WIN.blit(right_text, right_text_rect)
 
         pygame.display.update()
 
@@ -71,6 +100,8 @@ def start_screen():
             elif event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+
+    pygame.time.delay(500)  # Add a slight delay before returning to main game
 
 def restart_game():
     global player, start_time, elapsed_time, stars, star_count, star_add_increment, hit
@@ -154,13 +185,13 @@ def main():
                     break
 
             if hit:
-                lost_text = FONT.render("You Lost! Press R to Restart or ESC to Quit", 1, "white")
+                lost_text = FONT.render("You Lost! Press R to Restart or ESC to Quit", 1, (255, 255, 255))
                 WIN.blit(lost_text, (WIDTH/2 - lost_text.get_width()/2, HEIGHT/2 - lost_text.get_height()/2))
                 pygame.display.update()
                 pygame.time.delay(2000)  
 
             if elapsed_time > 40:
-                win_text = FONT.render("You Won! Press R to Restart or ESC to Quit", 1, "white")
+                win_text = FONT.render("You Won! Press R to Restart or ESC to Quit", 1, (255, 255, 255))
                 WIN.blit(win_text, (WIDTH/2 - win_text.get_width()/2, HEIGHT/2 - win_text.get_height()/2))
                 pygame.display.update()
                 pygame.time.delay(2000)  
